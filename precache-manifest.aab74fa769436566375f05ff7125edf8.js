fe792b1b0a5b71b37e9ea25c8f321570a63c2999
Pushkar Tandon <ptondon515@gmail.com>
2018-12-01 01:34:22 +0530

    Updates

---
+++ precache-manifest.aab74fa769436566375f05ff7125edf8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f27dd3cd5c6eae02208e529d1ced9e0",
+    "url": "/theevent/static/media/womensketch.0f27dd3c.jpg"
+  },
+  {
+    "revision": "adb2d0cb6f8796bff97b",
+    "url": "/theevent/static/css/main.f10d1fc6.chunk.css"
+  },
+  {
+    "revision": "cf9e5d467ceac623b42d",
+    "url": "/theevent/static/js/1.cf9e5d46.chunk.js"
+  },
+  {
+    "revision": "2da569b18329e55d6b14",
+    "url": "/theevent/static/js/runtime~main.2da569b1.js"
+  },
+  {
+    "revision": "02faf9b6614e98ebfc805a941e9ecd13",
+    "url": "/theevent/static/media/slide_one.02faf9b6.jpeg"
+  },
+  {
+    "revision": "688f60a84817157aed97568a3c0c5716",
+    "url": "/theevent/static/media/slide_two.688f60a8.jpeg"
+  },
+  {
+    "revision": "71e4d0d94122317491db897872c048c0",
+    "url": "/theevent/static/media/slide_three.71e4d0d9.jpeg"
+  },
+  {
+    "revision": "55b024e1542935a963356618edc0978b",
+    "url": "/theevent/static/media/hrithiksketch.55b024e1.jpg"
+  },
+  {
+    "revision": "adb2d0cb6f8796bff97b",
+    "url": "/theevent/static/js/main.adb2d0cb.chunk.js"
+  },
+  {
+    "revision": "28f80d94d9fb38fd92569e56e55db6eb",
+    "url": "/theevent/static/media/ironmansketch.28f80d94.jpg"
+  },
+  {
+    "revision": "9eb7d6212ed56c2031882dadceadfcd9",
+    "url": "/theevent/static/media/handsketch.9eb7d621.jpg"
+  },
+  {
+    "revision": "4f2b19481da72fcea5c291b9eaee0970",
+    "url": "/theevent/static/media/sketch5.4f2b1948.jpg"
+  },
+  {
+    "revision": "40b7360eae88f44aca08081a742ab714",
+    "url": "/theevent/static/media/sketch6.40b7360e.jpg"
+  },
+  {
+    "revision": "17a8361c9f91eddf62d7733e27bf3d46",
+    "url": "/theevent/static/media/sketch7.17a8361c.jpg"
+  },
+  {
+    "revision": "1e675e87d3eac3d923d73c74196b6c45",
+    "url": "/theevent/static/media/sketch8.1e675e87.jpg"
+  },
+  {
+    "revision": "66faec99be009c392580479a077e2208",
+    "url": "/theevent/static/media/sketch9.66faec99.jpg"
+  },
+  {
+    "revision": "152ca4fea57f0f73ba87a25c79e98903",
+    "url": "/theevent/index.html"
+  }
+];
